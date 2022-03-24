771ab36c2a62fb7e9c5e773eb19f96d0a6a7ed24
yfcmrbyc <78963995+yfcmrbyc@users.noreply.github.com>
2022-03-24 18:50:28 +0300

    Update helper-functions.js

--- src/reducers/helper-functions.js
+++ src/reducers/helper-functions.js
@@ -1,383 +1,90 @@
 import { VisibilitySorting } from '../actions/actions';
 
-export const changeFilter = (currentFilter, filters, tickets) => {
-  const { all, otherFilters } = filters;
-
-  const hasAllFalse = {
-    filters: {
-      all: false,
-      otherFilters: {
-        nonStop: false,
-        one: false,
-        two: false,
-        three: false,
-      },
-    },
-    tickets: [],
-  };
-
-  const hasAllTrue = {
-    filters: {
-      all: true,
-      otherFilters: {
-        nonStop: true,
-        one: true,
-        two: true,
-        three: true,
-      },
-    },
-    tickets,
-  };
-
-  if (currentFilter === 'all') {
-    if (all) return hasAllFalse;
-    else return hasAllTrue;
-  } else {
-    const immutableProperties = Object.getOwnPropertyNames(otherFilters).filter((item) => item !== currentFilter);
-    const isChecked = immutableProperties.every((item) => otherFilters[item] === true);
-    const filterTickets = (value) =>
-      tickets.filter(
-        (ticket) => ticket.segments[0].stops.length === value || ticket.segments[0].stops.length === value
-      );
-
-    if (isChecked && otherFilters[currentFilter]) {
-      const tempFilters = {
+export const changeFilter = (currentFilter, filters, tickets, filteredTickets) => {
+    const { all, otherFilters } = filters;
+  
+    const hasAllFalse = {
+      filters: {
         all: false,
         otherFilters: {
-          ...otherFilters,
-          [currentFilter]: false,
+          nonStop: { value: 0, checked: false },
+          one: { value: 1, checked: false },
+          two: { value: 2, checked: false },
+          three: { value: 3, checked: false },
         },
-      };
-
-      switch (currentFilter) {
-        case 'nonStop':
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(1), ...filterTickets(2), ...filterTickets(3)],
-          };
-        case 'one':
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(0), ...filterTickets(2), ...filterTickets(3)],
-          };
-        case 'two':
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(0), ...filterTickets(1), ...filterTickets(3)],
-          };
-        case 'three':
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(0), ...filterTickets(1), ...filterTickets(2)],
-          };
-      }
-    } else if (isChecked && !otherFilters[currentFilter]) {
-      return hasAllTrue;
-    } else {
-      const tempFilters = {
-        all,
+      },
+      tickets: [],
+    };
+  
+    const hasAllTrue = {
+      filters: {
+        all: true,
         otherFilters: {
-          ...otherFilters,
-          [currentFilter]: !otherFilters[currentFilter],
+          nonStop: { value: 0, checked: true },
+          one: { value: 1, checked: true },
+          two: { value: 2, checked: true },
+          three: { value: 3, checked: true },
         },
+      },
+      tickets,
+    };
+  
+    if (currentFilter === 'all') {
+      if (all) return hasAllFalse;
+      else return hasAllTrue;
+    } else {
+      const isChecked = Object.getOwnPropertyNames(otherFilters)
+                              .filter((item) => item !== currentFilter)
+                              .every((item) => otherFilters[item].selected === true);
+      const filterTickets = (filter) => {
+        if (otherFilters[filter].checked) {
+          return [
+            ...filteredTickets,
+            tickets.filter(ticket => 
+                           ticket.segments[0].stops.length === otherFilters[filter].value || ticket.segments[0].stops.length === otherFilters[filter].value)
+          ]
+        } else {
+          return [
+            filteredTickets.filter(ticket => 
+                           ticket.segments[0].stops.length !== otherFilters[filter].value || ticket.segments[0].stops.length !== otherFilters[filter].value)
+          ]
+        }
       };
-
-      const { nonStop, one, two, three } = otherFilters;
-
-      if (currentFilter === 'nonStop' && !otherFilters[currentFilter]) {
-        if (!one && !two && !three) {
-          return {
-            filters: tempFilters,
-            tickets: filterTickets(0),
-          };
-        } else if (one && !two && !three) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(0), ...filterTickets(1)],
-          };
-        } else if (!one && two && !three) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(0), ...filterTickets(2)],
-          };
-        } else if (!one && !two && three) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(0), ...filterTickets(3)],
-          };
-        } else if (one && two && !three) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(0), ...filterTickets(1), ...filterTickets(2)],
-          };
-        } else if (one && !two && three) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(0), ...filterTickets(1), ...filterTickets(3)],
-          };
-        } else if (!one && two && three) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(0), ...filterTickets(2), ...filterTickets(3)],
-          };
+  
+  
+      if (isChecked && otherFilters[currentFilter].checked) {
+        return {
+          filters: {
+            all: false,
+            otherFilters: {
+              ...otherFilters,
+              [currentFilter]: {
+                value,
+                checked: false 
+              }
+           }
+        },
+            tickets: filterTickets(currentFilter)      
         }
-      } else if (currentFilter === 'nonStop' && otherFilters[currentFilter]) {
-        if (!one && !two && !three) {
-          return hasAllFalse;
-        } else if (one && !two && !three) {
-          return {
-            filters: tempFilters,
-            tickets: filterTickets(1),
-          };
-        } else if (!one && two && !three) {
-          return {
-            filters: tempFilters,
-            tickets: filterTickets(2),
-          };
-        } else if (!one && !two && three) {
-          return {
-            filters: tempFilters,
-            tickets: filterTickets(3),
-          };
-        } else if (one && two && !three) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(1), ...filterTickets(2)],
-          };
-        } else if (one && !two && three) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(1), ...filterTickets(3)],
-          };
-        } else if (!one && two && three) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(2), ...filterTickets(3)],
-          };
+      } else if (isChecked && !otherFilters[currentFilter].checked) {
+        return hasAllTrue;
+      } else {
+        return {
+          filters: {  
+            all,
+            otherFilters: {
+              ...otherFilters,
+              [currentFilter]: {
+                  value,
+                  checked: !otherFilters[currentFilter].checked 
+                }
+            }
+          },
+          tickets: filterTickets(currentFilter) 
         }
       }
-
-      if (currentFilter === 'one' && !otherFilters[currentFilter]) {
-        if (!nonStop && !two && !three) {
-          return {
-            filters: tempFilters,
-            tickets: filterTickets(1),
-          };
-        } else if (nonStop && !two && !three) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(0), ...filterTickets(1)],
-          };
-        } else if (!nonStop && two && !three) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(1), ...filterTickets(2)],
-          };
-        } else if (!nonStop && !two && three) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(1), ...filterTickets(3)],
-          };
-        } else if (nonStop && two && !three) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(0), ...filterTickets(1), ...filterTickets(2)],
-          };
-        } else if (nonStop && !two && three) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(0), ...filterTickets(1), ...filterTickets(3)],
-          };
-        } else if (!nonStop && two && three) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(1), ...filterTickets(2), ...filterTickets(3)],
-          };
-        }
-      } else if (currentFilter === 'one' && otherFilters[currentFilter]) {
-        if (!nonStop && !two && !three) {
-          return hasAllFalse;
-        } else if (nonStop && !two && !three) {
-          return {
-            filters: tempFilters,
-            tickets: filterTickets(0),
-          };
-        } else if (!nonStop && two && !three) {
-          return {
-            filters: tempFilters,
-            tickets: filterTickets(2),
-          };
-        } else if (!nonStop && !two && three) {
-          return {
-            filters: tempFilters,
-            tickets: filterTickets(3),
-          };
-        } else if (nonStop && two && !three) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(0), ...filterTickets(2)],
-          };
-        } else if (nonStop && !two && three) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(0), ...filterTickets(3)],
-          };
-        } else if (!nonStop && two && three) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(2), ...filterTickets(3)],
-          };
-        }
-      }
-
-      if (currentFilter === 'two' && !otherFilters[currentFilter]) {
-        if (!nonStop && !one && !three) {
-          return {
-            filters: tempFilters,
-            tickets: filterTickets(2),
-          };
-        } else if (nonStop && !one && !three) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(0), ...filterTickets(2)],
-          };
-        } else if (!nonStop && one && !three) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(1), ...filterTickets(2)],
-          };
-        } else if (!nonStop && !one && three) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(2), ...filterTickets(3)],
-          };
-        } else if (nonStop && one && !three) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(0), ...filterTickets(1), ...filterTickets(2)],
-          };
-        } else if (nonStop && !one && three) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(0), ...filterTickets(2), ...filterTickets(3)],
-          };
-        } else if (!nonStop && one && three) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(1), ...filterTickets(2), ...filterTickets(3)],
-          };
-        }
-      } else if (currentFilter === 'two' && otherFilters[currentFilter]) {
-        if (!nonStop && !one && !three) {
-          return hasAllFalse;
-        } else if (nonStop && !one && !three) {
-          return {
-            filters: tempFilters,
-            tickets: filterTickets(0),
-          };
-        } else if (!nonStop && one && !three) {
-          return {
-            filters: tempFilters,
-            tickets: filterTickets(1),
-          };
-        } else if (!nonStop && !one && three) {
-          return {
-            filters: tempFilters,
-            tickets: filterTickets(3),
-          };
-        } else if (nonStop && one && !three) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(0), ...filterTickets(1)],
-          };
-        } else if (nonStop && !one && three) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(0), ...filterTickets(3)],
-          };
-        } else if (!nonStop && one && three) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(1), ...filterTickets(3)],
-          };
-        }
-      }
-
-      if (currentFilter === 'three' && !otherFilters[currentFilter]) {
-        if (!nonStop && !one && !two) {
-          return {
-            filters: tempFilters,
-            tickets: filterTickets(3),
-          };
-        } else if (nonStop && !one && !two) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(0), ...filterTickets(3)],
-          };
-        } else if (!nonStop && one && !two) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(1), ...filterTickets(3)],
-          };
-        } else if (!nonStop && !one && two) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(2), ...filterTickets(3)],
-          };
-        } else if (nonStop && one && !two) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(0), ...filterTickets(1), ...filterTickets(3)],
-          };
-        } else if (nonStop && !one && two) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(0), ...filterTickets(2), ...filterTickets(3)],
-          };
-        } else if (!nonStop && one && two) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(1), ...filterTickets(2), ...filterTickets(3)],
-          };
-        }
-      } else if (currentFilter === 'three' && otherFilters[currentFilter]) {
-        if (!nonStop && !one && !two) {
-          return hasAllFalse;
-        } else if (nonStop && !one && !two) {
-          return {
-            filters: tempFilters,
-            tickets: filterTickets(0),
-          };
-        } else if (!nonStop && one && !two) {
-          return {
-            filters: tempFilters,
-            tickets: filterTickets(1),
-          };
-        } else if (!nonStop && !one && two) {
-          return {
-            filters: tempFilters,
-            tickets: filterTickets(2),
-          };
-        } else if (nonStop && one && !two) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(0), ...filterTickets(1)],
-          };
-        } else if (nonStop && !one && two) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(0), ...filterTickets(2)],
-          };
-        } else if (!nonStop && one && two) {
-          return {
-            filters: tempFilters,
-            tickets: [...filterTickets(1), ...filterTickets(2)],
-          };
-        }
-      }
-    }
-  }
-};
+    };
+}
 
 export const changeSorting = (sort, arr) => {
   const arrForSort = [...arr];
